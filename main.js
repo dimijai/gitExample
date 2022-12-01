3879cb78dfbb0e4520ef14238c8a67c1d9014413
dimijai <df023422@gmail.com>
2022-12-01 22:08:08 +0530

    final1

--- main.js
+++ main.js
@@ -15,26 +15,33 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newDes = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
+
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" " + newDes));
 
-  // Create del button element
+  // Create del & edit button element
   var deleteBtn = document.createElement('button');
   var editBtn = document.createElement('button');
 
-  // Add classes to del button
+  // Add classes to del & edit button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editBtn.className = 'btn btn-sm float-right editBtn';
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('EDIT'));
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+
 
   // Append li to list
   itemList.appendChild(li);
@@ -54,12 +61,13 @@ function removeItem(e){
 function filterItems(e){
   // convert text to lowercase  
   var text = e.target.value.toLowerCase();
-  // Get lis
+  // Get list
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1 ){
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
